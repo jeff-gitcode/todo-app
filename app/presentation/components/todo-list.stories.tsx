62e8f2400c129ee5e70bfc94315f8abcf5166e9b
jeff-gitcode <jeff.gitcode@gmail.com>
2025-03-07 22:10:40 +1100

    updated storybook for todolist

--- app/presentation/components/todo-list.stories.tsx
+++ app/presentation/components/todo-list.stories.tsx
@@ -1,15 +1,17 @@
 import React, { act } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 import TodoList from './todo-list';
-import { fn } from '@storybook/test';
+import { fn, waitFor, within, expect } from '@storybook/test';
 import * as actual from '../hooks/use-todos';
+import { createMock, getMock, render } from 'storybook-addon-module-mock';
+import { redirect, getRouter } from '@storybook/nextjs/navigation.mock';
 
 const routerPush = fn();
 
 const mockTodos = [
-  { id: 1, title: 'Todo 1', completed: false, createdAt: new Date(), updatedAt: new Date(), createdAt: new Date(), updatedAt: new Date() },
-  { id: 2, title: 'Todo 2', completed: false, createdAt: new Date(), updatedAt: new Date(), createdAt: new Date(), updatedAt: new Date() },
-  { id: 3, title: 'Todo 3', completed: false, createdAt: new Date(), updatedAt: new Date(), createdAt: new Date(), updatedAt: new Date() },
+  { id: 1, title: 'Todo 1', completed: false, createdAt: new Date(), updatedAt: new Date() },
+  { id: 2, title: 'Todo 2', completed: false, createdAt: new Date(), updatedAt: new Date() },
+  { id: 3, title: 'Todo 3', completed: false, createdAt: new Date(), updatedAt: new Date() },
 ];
 
 const useTodos = fn(actual.useTodos).mockName('useTodos');
@@ -24,15 +26,16 @@ const meta = {
     nextjs: {
       // 👇 As in the Next.js application, next/navigation only works using App Router
       appDirectory: true,
-      router: {
-        push: routerPush,
-        asPath: '/presentation/pages/protected/todos',
-        pathname: '/presentation/pages/protected/todos',
-        query: {},
-      },
+      // router: {
+      //   push: routerPush,
+      //   asPath: '/',
+      //   pathname: '/',
+      //   query: {},
+      //   redirect: fn(),
+      //   getRouter: fn(),
+      // },
     },
     jest: ['todo-list.test.tsx'],
-
   },
 } satisfies Meta<typeof TodoList>;;
 
@@ -47,8 +50,44 @@ export const Default: Story = {
     useTodos.mockClear();
     useDeleteTodo.mockClear();
 
-    useTodos.mockReturnValue({ data: mockTodos, isLoading: false, error: null });
-    useDeleteTodo.mockReturnValue({
+    // useTodos.mockReturnValue({ data: mockTodos, isLoading: false, error: null });
+    // useDeleteTodo.mockReturnValue({
+    //   mutate: fn(),
+    //   data: undefined,
+    //   error: null,
+    //   variables: undefined,
+    //   isError: false,
+    //   isLoading: false,
+    //   isSuccess: false,
+    // });
+  },
+  args: {
+    // Provide default props here if needed
+  },
+  parameters: {
+    nextjs: {
+      // appDirectory: true,
+      router: {
+        push: routerPush,
+      },
+    },
+    moduleMock: {
+      mock: () => {
+        const useTodos = actual.useTodos;
+        const useDeleteTodo = actual.useDeleteTodo;
+        const mock = createMock(actual, 'useTodos');
+        mock.mockImplementation(useTodos);
+        const mock2 = createMock(actual, 'useDeleteTodo');
+        mock2.mockImplementation(useDeleteTodo);
+        return [mock, mock2];
+      },
+    },
+  },
+  play: async ({ canvasElement, parameters }) => {
+    const mock = getMock(parameters, actual, 'useTodos');
+    mock.mockImplementation(() => ({ data: mockTodos, isLoading: false, error: null }));
+    const mock2 = getMock(parameters, actual, 'useDeleteTodo');
+    mock2.mockImplementation(() => ({
       mutate: fn(),
       data: undefined,
       error: null,
@@ -56,9 +95,31 @@ export const Default: Story = {
       isError: false,
       isLoading: false,
       isSuccess: false,
+    }));
+
+    render(parameters);
+    const canvas = within(canvasElement);
+
+    await waitFor(async () => {
+      expect(mock).toBeCalled();
+      expect(mock2).toBeCalled();
     });
-  },
-  args: {
-    // Provide default props here if needed
+
+    const deleteButton = canvas.getAllByRole('button', { name: /Delete/i })[0];
+
+
+    // for (let i = 0; i < 1; i++) {
+    //   const deleteButton = canvas.getAllByRole('button', { name: /Delete/i })
+    //     .find((button) => button.id === i.toString());
+
+    expect(deleteButton).toBeDefined();
+
+    act(() => {
+      deleteButton!.click();
+    });
+
+    await waitFor(() => {
+      expect(mock2).toBeCalled();
+    });
   },
 };
